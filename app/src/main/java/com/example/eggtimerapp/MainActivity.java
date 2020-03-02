d47af25c8388620d47952f709984c080149823e0
Dave Song <49602639+woony91@users.noreply.github.com>
2020-03-02 11:31:51 -0500

    initialized egg timer app

---
+++ app/src/main/java/com/example/eggtimerapp/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.eggtimerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void startTimer(View view){
+        Log.i("info", "startTimer: clicked");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SeekBar timerSeekBar = findViewById(R.id.sb_timeSet);
+        final TextView timerTextView = findViewById(R.id.tv_timer);
+
+        timerSeekBar.setMax(600);
+        timerSeekBar.setProgress(30);
+
+        timerSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int minutes = progress / 60;
+                int seconds = progress - (minutes * 60);
+
+                String secondString = Integer.toString(seconds);
+                if (secondString.equals("0")){
+                    secondString = "00";
+                }
+
+                timerTextView.setText(Integer.toString(minutes) + ":" + secondString);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
